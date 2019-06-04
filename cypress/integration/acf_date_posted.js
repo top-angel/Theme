2048b4cd732ac70d09d16a503e13f8ece4171188
top-angel <ddwebdev2019@gmail.com>
2019-06-04 16:39:13 -0700

    got date posted value on backend to work

--- cypress/integration/acf_date_posted.js
+++ cypress/integration/acf_date_posted.js
@@ -23,6 +23,7 @@ describe('acf_date_posted', ()=>{
         cy.get('a.page-title-action').click();
         //We should see that are at the place where we can add a new Post
         cy.contains('Add New Post');
+        cy.get('input#title').type('acf_date_posted');
         //We see that our ACF Group is present
         cy.get('#acf-group_5cf2e7687784b').find('h2').find('span').should('contain', 'Blog Post Group');
         cy.get('.acf-label').find('label').should('contain', 'Date Posted');
@@ -39,6 +40,24 @@ describe('acf_date_posted', ()=>{
     });
 
     it('should allow you to select a year', ()=>{
-        
+        cy.get('select.ui-datepicker-year').select('1919');
+        cy.get('#ui-datepicker-div').should('contain', '1919');
+    });
+
+    it('should allow you to select a day', ()=>{
+        cy.get('table.ui-datepicker-calendar').find('a.ui-state-default').first().click();
+        cy.get('input.hasDatepicker').click();
+        cy.get('table.ui-datepicker-calendar').find('a.ui-state-active').should('contain','1');
+        //Cypress wil require you to get the colors in RGB format from the 'Computed' pane in Inspector Tools
+        cy.get('table.ui-datepicker-calendar').find('a.ui-state-active').should('have.css', 'background-color', "rgb(56, 117, 215)");
+        //now we close the pane
+        cy.get('button.ui-datepicker-close').click();        
+    });
+
+    it('should read January 1, 1919', ()=>{
+        cy.get('input#publish').click(); 
+        cy.wait(2000);
+        cy.get('input.hasDatepicker').should('have.value', 'January 1, 1919');
     })
+
 })

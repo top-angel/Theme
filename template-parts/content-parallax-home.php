cf0a92693f66f5f16e8fe9f61e35003467910fc4
top-angel <ddwebdev2019@gmail.com>
2019-07-30 11:14:13 -0700

    going to gym

--- template-parts/content-parallax-home.php
+++ template-parts/content-parallax-home.php
@@ -1,6 +1,8 @@
 <?php 
 $image1 = wp_get_attachment_image_src(278, full);
 $image2 = wp_get_attachment_image_src(279, full);
+$image3 = wp_get_attachment_image_src(280, full);
+
 ?>
 <div id="content-parallax-container">
 <!--TODO:How can we get images from SCSS into the fucking thing here? Maybe use a backup CSS file?-->
@@ -22,10 +24,24 @@ $image2 = wp_get_attachment_image_src(279, full);
         style="background-image:url(<?php echo $image2[0] ?>"
     >
         <h2 class="mediumFont indexCategorySection">ABOUT</h2>
-        <h1>Nancy is the shit, she has the power to make you sing like an ox that has 3 eyes, elevent hands
-            30 legs, and 3 teeth. It will fuck you up and make people cry, but in a good soul enriching way that makes you
-            go gansna gansna smoke more weed.
-</h1>
-    </section>
+        <p> 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius pulvinar justo. Nulla ac mi sed lacus tempor vehicula. Vestibulum vitae elit ut erat varius vehicula. Donec nisl nunc, vestibulum vitae cursus ut, auctor eu dolor. Sed tristique semper risus, ut tristique quam sagittis vel. Nullam vel ultrices leo. Mauris facilisis quam enim, ut consequat sem commodo vestibulum. Duis id nisi nisi. Aenean vel dignissim ex, sed volutpat diam. Fusce maximus consequat ornare. Nunc in odio at magna ullamcorper vestibulum. Integer scelerisque augue ligula, non rutrum justo dapibus sit amet. Phasellus at scelerisque erat, quis ullamcorper diam. Integer fermentum porttitor libero pulvinar maximus.
+
+Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras pharetra porttitor pretium. Nulla finibus orci ut leo fringilla rutrum. Cras ligula elit, ultrices id vulputate eu, malesuada et dui. Vivamus turpis felis, facilisis nec convallis et, accumsan fringilla ante. Aenean laoreet lorem justo. Donec iaculis rhoncus laoreet. Nulla nisi augue, feugiat eget molestie id, consectetur in ex. Curabitur eget urna feugiat, euismod eros ut, dictum enim. In hac habitasse platea dictumst. Integer nec lectus id dolor pulvinar rutrum. Praesent ut dignissim felis. Curabitur rhoncus ipsum nec maximus blandit.
+</p>
 
 </div>
+
+    <div class="parallax"style='background-image:url(<?php echo $image1[0] ?>); min-height: 500px;
+    height: 30vh;'
+    >   
+    </div>
+
+    <div 
+    id="contact-parallax-container" class='parallax'
+    style="background-image:url(<?php echo $image3[0] ?>"
+    >
+        <h2>CONTACT</h2>
+        <p>wubmo</p>
+
+    </div>

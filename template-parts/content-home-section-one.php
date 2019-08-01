e91418788c1fabed4787347de6c4b60ee5d86116
top-angel <ddwebdev2019@gmail.com>
2019-08-01 01:00:26 -0700

    sayonara

--- template-parts/content-home-section-one.php
+++ template-parts/content-home-section-one.php
@@ -24,10 +24,6 @@
             $newsItemDate = get_the_date();
             $newsItemAuthor = get_the_author();
             $newsItemPictureSrc = wp_get_attachment_image_src(get_field('news_image', false, false), false);
-          
-            // echo $newsItemPictureSrc[0];
-            // echo '<img src=" ' . $newsItemPictureSrc[0] . ' " />';
-
         }
     
     ?>
@@ -37,7 +33,7 @@
                 <!-- News item first -->
                 <a 
                 style="background: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(<?php echo $newsItemPictureSrc[0] ?>)"
-                href="#" 
+                href="<?php echo esc_url(get_permalink()) ?>" 
                 class='nocStudioHomeFeedItem  list-group-item list-group-item-action active'>
                     <div class="nocStudioHomeFeedName d-flex w-100 justify-content-between">
                         <h5>NEWS</h5>
@@ -47,6 +43,39 @@
 
                 </a><!-- list-group-item list-group-item-action active -->
 
+                <?php wp_reset_postdata();
+                
+                $eventsArgs = array (
+                    'post_type'=> array('event'),
+                    'order' => 'DESC',
+                    'orderby' => 'date',
+                    'post_per_page' => '1'
+                );
+        
+                $eventItem = new WP_Query($eventsArgs);
+                if( $eventItem -> have_posts() ) {
+                    $eventItem -> the_post();
+        
+                    $eventItemTitle = get_the_title();
+                    $eventItemContent = get_the_content();
+                    $eventItemDate = get_the_date();
+                    $eventItemAuthor = get_the_author();
+                    $eventItemPictureSrc = wp_get_attachment_image_src(get_field('news_image', false, false), false);
+                }
+                
+                ?>
+
+                <a 
+                style="background: linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(<?php echo $eventItemPictureSrc[0] ?>)"
+                href="<?php echo esc_url(get_permalink()) ?>" 
+                class='nocStudioHomeFeedItem  list-group-item list-group-item-action active'>
+                    <div class="nocStudioHomeFeedName d-flex w-100 justify-content-between">
+                        <h5>EVENTS</h5>
+                    </div><!-- d-flex w-100 justify-content-between -->
+                    <p class="nocStudioHomeFeedTitle mb-1"><?php echo $eventItemTitle ?></p>
+                    <small class="nocStudioHomeFeedDate"><?php echo $eventItemDate ?></small>
+
+                </a><!-- list-group-item list-group-item-action active -->
 
             </div><!-- .list-group -->
     </div>
